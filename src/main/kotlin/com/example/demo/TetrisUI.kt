5a420f29b84a2a498aae12332d26a303a945f275
asant <asantursunaliev0606@gmail.com>
2024-04-26 14:16:03 +0600

    total

--- src/main/kotlin/com/example/demo/TetrisUI.kt
+++ src/main/kotlin/com/example/demo/TetrisUI.kt
@@ -4,13 +4,16 @@ import javafx.animation.AnimationTimer
 import javafx.application.Application
 import javafx.scene.Scene
 import javafx.scene.canvas.Canvas
+import javafx.scene.control.Alert
 import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
 import javafx.scene.input.KeyCode.*
 
 
 class TetrisUI : Application() {
+
     private val game = TetrisGame()
+
     override fun start(mainWindow: Stage) {
         val canvas = Canvas(TetrisGame.BLOCK_SIZE * TetrisGame.WIDTH, TetrisGame.BLOCK_SIZE * TetrisGame.HEIGHT)
         val gc = canvas.graphicsContext2D
@@ -19,26 +22,35 @@ class TetrisUI : Application() {
         val timer = object : AnimationTimer() {
             override fun handle(now: Long) {
                 if (now - game.lastMove > TetrisGame.SPEED) {
-                    game.lastMove = now
-                    game.update()
-                    game.draw(gc)
+                    with(game) {
+                        lastMove = now
+                        update()
+                        draw(gc)
+                    }
                 }
             }
         }
         timer.start()
 
-        val scene = Scene(borderPane)
-        mainWindow.scene = scene
-        mainWindow.title = "Tetris"
-        mainWindow.show()
+        with(mainWindow) {
+            scene = Scene(borderPane)
+            title = "Tetris"
+            show()
+        }
 
-        scene.setOnKeyPressed { event ->
+        mainWindow.scene.setOnKeyPressed { event ->
             when (event.code) {
                 LEFT -> game.moveLeft()
                 RIGHT -> game.moveRight()
                 DOWN -> game.moveDown()
                 UP -> game.rotatePiece()
-                else -> println("Unhandled key: ${event.code}")
+                else -> {
+                    with(Alert(Alert.AlertType.WARNING)) {
+                        title = "Error"
+                        contentText = "Incorrect key: ${event.code} Use keys only for game"
+                        showAndWait()
+                    }
+                }
             }
         }
     }
